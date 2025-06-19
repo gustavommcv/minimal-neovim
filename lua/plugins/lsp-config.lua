292c1031a099a863fb6b3fc92d7d4394200177ba
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-06-19 20:56:30 -0300

    refactor shortcut

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,66 +3,67 @@
 -- mason-lspconfig
 -- nvim-lspconfig
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "ts_ls",
-          "bashls",
-          "angularls",
-          "cssls",
-          "emmet_language_server",
-          "gopls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig/util")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"bashls",
+					"angularls",
+					"cssls",
+					"emmet_language_server",
+					"gopls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			-- local util = require("lspconfig/util")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.angularls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.angularls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.emmet_language_server.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.emmet_language_server.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+		end,
+	},
 }
