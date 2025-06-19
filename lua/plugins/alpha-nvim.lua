95ac57ed5880cc2e190dcbc953fb3e7913bc7c22
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-06-19 19:23:48 -0300

    custom alpha nvim config

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -2,10 +2,46 @@
 -- https://github.com/goolord/alpha-nvim
 return {
 	"goolord/alpha-nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "echasnovski/mini.icons" },
+	event = "VimEnter",
+	dependencies = { "RchrdAriza/nvim-web-devicons" },
 	config = function()
-		require("alpha").setup(require("alpha.themes.dashboard").config)
-		local startify = require("alpha.themes.startify")
-		startify.file_icons.provider = "devicons"
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		-- Set header
+		dashboard.section.header.val = {
+			"       ███╗░░██╗██╗░░░██╗██╗███╗░░░███╗",
+			"       ████╗░██║██║░░░██║██║████╗░████║",
+			"       ██╔██╗██║╚██╗░██╔╝██║██╔████╔██║",
+			"       ██║╚████║░╚████╔╝░██║██║╚██╔╝██║",
+			"       ██║░╚███║░░╚██╔╝░░██║██║░╚═╝░██║",
+			"       ╚═╝░░╚══╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝",
+			"",
+			"                 █▀█ █▄░█  █▀▄▀█ █▄█            ",
+			"                 █▄█ █░▀█  █░▀░█ ░█░            ",
+			"",
+			"       ░██╗░░░░░░░██╗░█████╗░██╗░░░██╗",
+			"       ░██║░░██╗░░██║██╔══██╗╚██╗░██╔╝",
+			"       ░╚██╗████╗██╔╝███████║░╚████╔╝░",
+			"       ░░████╔═████║░██╔══██║░░╚██╔╝░░",
+			"       ░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░",
+			"       ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░",
+		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("n", "   New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "󰮗   Find file", ":cd $HOME | Telescope find_files<CR>"),
+			dashboard.button("e", "   File Explorer", ":cd $HOME | Neotree<CR>"),
+			dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("c", "   Configuration", ":e ~/.config/nvim/lua/user/config.lua<CR>"),
+			dashboard.button("R", "󱘞   Ripgrep", ":Telescope live_grep<CR>"),
+			dashboard.button("q", "󰗼   Quit", ":qa<CR>"),
+		}
+
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
+
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
 	end,
 }
