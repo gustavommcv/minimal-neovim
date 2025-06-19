ff19317f7f32b6ccda999765bbee89e38cb8eba6
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-06-19 17:53:04 -0300

    inline messages

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -12,3 +12,14 @@ opt.undodir = vim.fn.stdpath("config") .. "/undodir//"
 vim.fn.mkdir(vim.fn.stdpath("config") .. "/undodir", "p")
 
 opt.cursorline = true
+
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "●",
+		spacing = 2,
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
