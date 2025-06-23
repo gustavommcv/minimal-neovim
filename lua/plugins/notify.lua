788beab35cd94a3851f9924f883a0e5b883bbe63
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-06-23 18:23:23 -0300

    Add more specific notify config

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,13 @@
+-- A fancy, configurable, notification manager for NeoVim
+-- https://github.com/rcarriga/nvim-notify
+return {
+	"rcarriga/nvim-notify",
+	config = function()
+		require("notify").setup({
+			render = "simple",
+			top_left = false,
+		})
+
+		vim.notify = require("notify")
+	end,
+}
