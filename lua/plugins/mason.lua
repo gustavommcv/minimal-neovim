992a7f1e44e136048805f0b776112170268e5236
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-08-12 14:02:15 -0300

    css lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -13,7 +13,7 @@ return {
 		},
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "gopls", "dockerls", "somesass_ls", "angularls", "html" },
+				ensure_installed = { "lua_ls", "ts_ls", "gopls", "dockerls", "somesass_ls", "angularls", "html", "cssls" },
 				handlers = {
 					function(server_name) -- default
 						require("lspconfig")[server_name].setup({})
