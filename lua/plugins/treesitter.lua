26b28be268b73f6a9d25ba8fa4a86eed54ad61f0
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-09-04 21:17:35 -0300

    treesitter golang

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "go" },
 
       highlight = {
         enable = true,
