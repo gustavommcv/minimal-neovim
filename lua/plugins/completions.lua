0a351ac011aefde44a5b53f933457d9e2522c47c
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-11-19 11:25:12 -0300

    Add tex snippets

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -33,9 +33,7 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load({
-				exclude = { "tex" },
-			})
+			require("luasnip.loaders.from_vscode").lazy_load({})
 
 			cmp.setup({
 				snippet = {
